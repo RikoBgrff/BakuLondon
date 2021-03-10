6985d4459e66ef7ea49f9f0f73ae0c3c180b38b7
RikoBgrff <riko.bagirli777@gmail.com>
2021-03-11 00:35:05 +0400

    Add project files.

---
+++ BakuLondon/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BakuLondon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bakuBtn = new System.Windows.Forms.Button();
+            this.londonBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bakuBtn
+            // 
+            this.bakuBtn.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.bakuBtn.Location = new System.Drawing.Point(65, 291);
+            this.bakuBtn.Name = "bakuBtn";
+            this.bakuBtn.Size = new System.Drawing.Size(93, 47);
+            this.bakuBtn.TabIndex = 0;
+            this.bakuBtn.Text = "Baku";
+            this.bakuBtn.UseVisualStyleBackColor = true;
+            this.bakuBtn.Click += new System.EventHandler(this.BakuBtn_Click);
+            // 
+            // londonBtn
+            // 
+            this.londonBtn.Location = new System.Drawing.Point(420, 291);
+            this.londonBtn.Name = "londonBtn";
+            this.londonBtn.Size = new System.Drawing.Size(93, 47);
+            this.londonBtn.TabIndex = 1;
+            this.londonBtn.Text = "London";
+            this.londonBtn.UseVisualStyleBackColor = true;
+            this.londonBtn.Click += new System.EventHandler(this.londonBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BakuLondon.Properties.Resources.baku2;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.londonBtn);
+            this.Controls.Add(this.bakuBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BakuLondon";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bakuBtn;
+        private System.Windows.Forms.Button londonBtn;
+    }
+}
+
---
+++ BakuLondon/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BakuLondon
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        Label label = new Label();
+
+        public Form1()
+        {
+            timer.Interval = 500;
+            timer.Start();
+            InitializeComponent();
+            CreateLabel();
+        }
+    void CreateLabel()
+    {
+            Image image = Properties.Resources.baku2;
+            Image image2 = Properties.Resources.london2;
+            label.Size = new Size(image.Width, image.Height);
+            label.Image = image2;
+            label.Image = image;
+            this.Controls.Add(label);
+        }
+
+        private void BakuBtn_Click(object sender, EventArgs e)
+        {
+            this.bakuBtn.BackColor = Color.DeepSkyBlue;
+            this.londonBtn.BackColor = Color.Empty;
+            Image image = Properties.Resources.baku2;
+            label.Image = image;
+            Text = $"Baku:{DateTime.Now.ToLongTimeString()}";
+        }
+
+        private void londonBtn_Click(object sender, EventArgs e)
+        {
+            this.bakuBtn.BackColor = Color.Empty;
+            this.londonBtn.BackColor = Color.SkyBlue;
+            Image image2 = Properties.Resources.london2;
+            label.Image = image2;
+            Text = $"London:{DateTime.Now.ToLongTimeString()}";
+            label.Image = image2;
+        }
+    }
+}
